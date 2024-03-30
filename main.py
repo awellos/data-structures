caf58c9bf1a3ae55901410ab54cb5dedee0ca0cb
awellos <160076984+awellos@users.noreply.github.com>
2024-03-30 11:30:13 +0000

    Updating the code

---
+++ main.py
@@ -0,0 +1,17 @@
+#creating a list called my_list
+my_list =[]
+#appending items to the list
+my_list.append(10)
+my_list.append(20)
+my_list.append(30)
+my_list.append(40)
+#adding a value to the second position in the list
+my_list.insert(1,15)
+#extending my_list 
+my_list.extend([50,60,70])
+#removing thelast element from my_list
+del my_list[-1]
+#sorting my_list in ascending Order
+my_list.sort()
+#finding and printing index of 30
+print(my_list.index(30))
